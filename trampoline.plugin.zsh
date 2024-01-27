1f135801a40440812d40a3bee76f918f1f819ccb
Hernán Cervera <hjcerveram@hotmail.com>
2024-01-27 16:24:14 -0600

    refactor: remove redundant file

--- trampoline.plugin.zsh
+++ trampoline.plugin.zsh
@@ -1,6 +1,13 @@
-# According to the standard:
+# zsh-trampoline -- Jump to the places that matter to you.
+# https://github.com/HerCerM/zsh-trampoline
+
 # https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-source ${0:h}/trampoline.zsh
+# Do not source this script multiple times.
+command -v zt_version > /dev/null && return
+
+source ${0:h}/src/configuration.zsh
+source ${0:h}/src/completions.zsh
+source ${0:h}/src/widgets.zsh
--- trampoline.zsh
+++
@@ -1,9 +0,0 @@
-# zsh-trampoline -- Jump to the places that matter to you.
-# https://github.com/HerCerM/zsh-trampoline
-
-# Do not source this script multiple times.
-command -v zt_version > /dev/null && return
-
-source ${0:a:h}/src/configuration.zsh
-source ${0:a:h}/src/completions.zsh
-source ${0:a:h}/src/widgets.zsh
